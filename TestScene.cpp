c63bcf6e03b10cf9e981294066e42999ecf02d5a
優吾 中林 <220159@jc-21.jp>
2023-11-07 11:38:20 +0900

    プロジェクト ファイルを追加します。

---
+++ TestScene.cpp
@@ -0,0 +1,30 @@
+#include "TestScene.h"
+#include"Engine/Input.h"
+#include "Engine/SceneManager.h"
+#include"Stage.h"
+//コンストラクタ
+TestScene::TestScene(GameObject* parent)
+	: GameObject(parent, "TestScene")
+{
+}
+
+//初期化
+void TestScene::Initialize()
+{
+}
+
+//更新
+void TestScene::Update()
+{
+	Instantiate<Stage>(this);
+}
+
+//描画
+void TestScene::Draw()
+{
+}
+
+//開放
+void TestScene::Release()
+{
+}
