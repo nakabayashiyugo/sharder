14db085d0604c8a2277a2683264b66902de6c9c1
優吾 中林 <220159@jc-21.jp>
2023-11-28 16:35:20 +0900

    pu-gi-

--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -26,9 +26,8 @@ struct VS_OUT
 	float4 pos    : SV_POSITION;	//位置
 	float2 uv	: TEXCOORD;	//UV座標
 	float4 color	: COLOR;	//色（明るさ）
+	float4 normal	: NORMAL;
 };
-
-
 
 //───────────────────────────────────────
 // 頂点シェーダ
@@ -45,7 +44,9 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 
 	//法線を回転
 	normal = mul(normal, matW);
-	
+
+	outData.normal = normal;
+
 	//light_vector = normalize(light_vector);
 	outData.color = clamp(dot(normal, light_vector), 0, 1);
 
@@ -70,34 +71,34 @@ float4 PS(VS_OUT inData) : SV_Target
 	if (isTexture == true) {
 		diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;
 		ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * float4(0.3, 0.3, 0.3, 1);
-		float4 r = 2 * matW * dot(matW, light_vector) - light_vector;
-		specular = (pow(dot(normalize(r), normalize(view_vector), n)) * Ks * lightSource;
+		float4 r = 2 * inData.normal * dot(normalize(inData.normal), normalize(light_vector)) - light_vector;
+		specular = pow(dot(normalize(r), normalize(view_vector)), n) * Ks * lightSource;
 	}
-	else {
+	else 
+	{
 		diffuse = lightSource * diffuseColor * inData.color;
 		ambient = lightSource * diffuseColor * float4(0.3, 0.3, 0.3, 1);
+		specular = lightSource * diffuseColor * float4(0.3, 0.3, 0.3, 1);
 	}
 	
+	return diffuse + specular + ambient;
 	
-	
-	return diffuse + ambient + supecular;
+	//float4 lightSource = float4(1.0, 1.0, 1.0, 1.0);
+	//float4 ambientSource = float4(0.2, 0.2, 0.2, 1.0);
+	//float4 diffuse;
+	//float4 ambient;
+	//if (isTexture == false)
+	//{
+	//	diffuse = lightSource * ambientSource * inData.color;
+	//	ambient = lightSource * ambientSource * inData.color;
+	//}
+	//else
+	//{
+	//	diffuse = lightSource * ambientSource * inData.color;
+	//	ambient = lightSource * ambientSource * inData.color;
+	//}
+	//float4 output = g_texture.Sample(g_sampler, inData.uv);
 
-	/*float4 lightSource = float4(1.0, 1.0, 1.0, 1.0);
-	float4 ambientSource = float4(0.2, 0.2, 0.2, 1.0);
-	float4 diffuse;
-	float4 ambient;
-	if (isTexture == false)
-	{
-		diffuse = lightSource * ambientSource * inData.color;
-		ambient = lightSource * ambientSource * inData.color;
-	}
-	else
-	{
-		diffuse = lightSource * ambientSource * inData.color;
-		ambient = lightSource * ambientSource * inData.color;
-	}*/
-	float4 output = g_texture.Sample(g_sampler, inData.uv);
-
-	return output;
+	//return output;
 }
 	
