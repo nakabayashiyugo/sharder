5cdde2ca2adf22964eaa4c1387193e3c1ca6bca1
優吾 中林 <220159@jc-21.jp>
2024-01-12 11:25:57 +0900

    輪郭できた

--- OutLine.hlsl
+++ OutLine.hlsl
@@ -45,26 +45,11 @@ struct VS_OUT
 //───────────────────────────────────────
 VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 {
-	//ピクセルシェーダーへ渡す情報
 	VS_OUT outData;
-
-	//ローカル座標に、ワールド・ビュー・プロジェクション行列をかけて
-	//スクリーン座標に変換し、ピクセルシェーダーへ
 	normal.w = 0;
-	normal = mul(normal, matNormal);
-	outData.normal = normal;
+	pos = pos + normal * 0.05f;
 	outData.pos = mul(pos, matWVP);
-	outData.pos += outData.normal * 0.05f;
-	outData.uv = uv;
-	
 
-	float4 light = normalize(lightPosition);
-
-	outData.color = saturate(dot(normal, light));
-	float4 posw = mul(pos, matW);
-	outData.eyev = eyePosition - posw;
-
-	//まとめて出力
 	return outData;
 }
 
@@ -73,34 +58,5 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 //───────────────────────────────────────
 float4 PS(VS_OUT inData) : SV_Target
 {
-	float4 lightSource = float4(0.0, 0.0, 0.0, 0.0);
-	float4 diffuse;
-	float4 ambient;
-	float4 NL = (dot(inData.normal, normalize(lightPosition)));
-	float4 reflect = normalize(2 * NL * inData.normal - normalize(lightPosition));
-
-	float2 uv;
-
-	uv.x = abs(dot(inData.normal, normalize(inData.eyev)));
-	uv.y = abs(dot(inData.normal, normalize(inData.eyev)));
-
-	float4 t1 = g_toon_texture.Sample(g_sampler, uv);
-
-	if (isTexture == true)
-	{
-		diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * t1;
-		ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambientColor;
-	}
-	else
-	{
-		diffuse = lightSource * diffuseColor * t1;
-		ambient = lightSource * diffuseColor * ambientColor;
-	}
-	float4 specular = float4(0, 0, 0, 0);
-	if (specularColor.a != 0)
-	{
-		specular = pow(saturate(dot(reflect, normalize(inData.eyev))), shininess);
-	}
-
-	return lightSource;
+	return float4(0, 0, 0, 1);
 }
